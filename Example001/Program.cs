6dd8283f454a704010ad3a7948c173e751ea5b7a
Inferno <1nf3rn1al@gmail.com>
2022-08-05 18:04:10 +0300

    add .gitignore

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,3 +1,4 @@
-﻿Console.ReadLine();
-Console.WriteLine("Hello, Artem!");
-
+﻿Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
