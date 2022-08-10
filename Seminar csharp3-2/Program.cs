eb194d20fc1a3f7a0c49f2c40ead048c83261e33
Inferno <1nf3rn1al@gmail.com>
2022-08-10 14:12:57 +0300

    homework c# 3 seminar

---
+++ Seminar csharp3-2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите координаты первой точки через энтер");
+int X1=int.Parse (Console.ReadLine());
+int Y1=int.Parse (Console.ReadLine());
+int Z1=int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки через энтер");
+int X2=int.Parse (Console.ReadLine());
+int Y2=int.Parse (Console.ReadLine());
+int Z2=int.Parse (Console.ReadLine());
+double max = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2));
+
+Console.WriteLine(Math.Round(max, 2));
