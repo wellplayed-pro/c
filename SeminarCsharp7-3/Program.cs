df0061b21fcbbcc600b1c55d4736af8ae403bba2
Inferno <1nf3rn1al@gmail.com>
2022-08-25 16:29:48 +0300

    Homework7

--- SeminarCsharp7-3/Program.cs
+++ SeminarCsharp7-3/Program.cs
@@ -1,23 +1,36 @@
 ﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
-void FillArray(int[,] array)
+int[,] FillArray(int[,] array)
 {
-  double middle=0;
+  
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
       array[i,j] = new Random().Next(10,51);
       Console.Write(array[i,j]+ " ");
-      middle = middle + array[i, j];
-
+      
     }
     Console.WriteLine(" ");
-    Console.WriteLine("Среднее арифметическое строчки " + (i+1) + " равно " +(middle /  array.GetLength(0)));
   }
- 
+  return array;
 }
+void Middle(int[,] array)
+{
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+    double middle = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      middle = middle + array[i, j];
+    }
+    Console.WriteLine("Среднее арифметическое столбца " + (j+1) + " равно " +(middle /  array.GetLength(0)));
+    }
+ }
+
+
 int[,] array = new int[4, 4];
 
-FillArray(array);
+Middle(FillArray(array));
 
